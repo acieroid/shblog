922b35af2009cbe27ddc51fd1694b38f346242e1
Vincent Lannurien <the.addikt1ve@gmail.com>
2010-05-31 11:33:06 +0200

    new markdown module using awk (no more perl deps)

--- lib/modules/markdown/markdown.sh
+++ lib/modules/markdown/markdown.sh
@@ -2,5 +2,5 @@
 
 function get_post {
 	POSTLINE=`cat $article | grep -n ^post:: | head -n 1 | cut -d: -f1`
-	cat $article | sed "1,"$POSTLINE"d" | $MODDIR/enabled/markdown/markdown.pl
+	cat $article | sed "1,"$POSTLINE"d" | $MODDIR/enabled/markdown/md2html.awk
 }
