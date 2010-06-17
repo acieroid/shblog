fc9c3ea6b77282a8a0c5d11bac8f513cf9cf679b
SigmaG <sigma_github@melix.net>
2010-06-17 19:35:04 +0200

    enabled comments in bloglist

--- lib/modules/enabled/blogroll/blogroll.sh
+++ lib/modules/enabled/blogroll/blogroll.sh
@@ -12,9 +12,12 @@ display_blogroll() {
 	echo "			<h3>Blogroll</h3>"
 	echo "			<ul id="blogroll">"
 	for blogline in $(get_blogroll); do
-		blog=`echo $blogline | cut -d' ' -f1`
-		link=`echo $blogline | cut -d' ' -f2`
-		echo "				<li><a href="$link">$blog</a></li>"
+		comment=$(echo $blogline | grep -E "[:space:]*#")
+		if [ "$comment" = "" ]; then
+			blog=`echo $blogline | cut -d' ' -f1`
+			link=`echo $blogline | cut -d' ' -f2`
+			echo "				<li><a href="$link">$blog</a></li>"
+		fi
 	done
 	echo "			</ul>"
 }
