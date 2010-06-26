c28393fe7591a06e3760d10bd8113fa4d5b7896c
Quentin Stievenart <acieroid@awesom.eu>
2010-06-26 12:37:57 +0200

    date's --rfc-2822 doesn't seem portable

--- lib/modules/rss/rss.sh
+++ lib/modules/rss/rss.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+RFC2822="%a, %d %b %Y %H:%M:%S %z"
+
 rss_gen() {
 	echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>"
 	echo "<rss version=\"2.0\">"
@@ -7,14 +9,14 @@ rss_gen() {
 	echo "		<title>$BLOGTITLE</title>"
 	echo "		<link>$BLOGURL</link>"
 	echo "		<description>$BLOGDESC</description>"
-	echo "		<lastBuildDate>`date --rfc-822`</lastBuildDate>"
+	echo "		<lastBuildDate>`date +"$RFC2822"`</lastBuildDate>"
 	echo "		<language>$BLOGLANG</language>"
 	for article in $(find_posts); do
 		echo "		<item>"
 		echo "			<title>$(get_title)</title>"
 		echo "			<link>$(get_www_link)</link>"
 		echo "			<guid>$(get_www_link)</guid>"
-		echo "			<pubDate>$(get_date --rfc-822)</pubDate>"
+		echo "			<pubDate>$(get_date +"$RFC2822")</pubDate>"
 		echo "			<description>$(get_post)</description>"
 		echo "		</item>"
 	done
